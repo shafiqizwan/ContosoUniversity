1513b1ddba51d3cfcce8b32c9d6bc0921c6b8c0d
UserShafiq <User@SGH626TFJP>
2016-11-19 16:22:52 +0800

    Add Login and Register.

---
+++ ContosoUniversity/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ContosoUniversity.Models;
+using static ContosoUniversity.Models.UsersContext;
+
+namespace ContosoUniversity.Controllers
+{
+    public class AccountController : Controller
+    {
+       public ActionResult Login (string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model, string returnUrl)
+        {
+            if(ModelState.IsValid)
+            {
+                if(Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if(Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {   
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The username of password is incorrect.");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Register model)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    MembershipUser NewUser = Membership.CreateUser(model.UserName, model.Password);
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("Registration Error", "Registration Error: " + e.StatusCode.ToString());
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ ContosoUniversity/Models/AccountModelClasses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace ContosoUniversity.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext() : base("SchoolContext")
+        {
+        }
+
+        public class Login
+        {
+            [Required] 
+            [Display(Name = "User name")]
+            public string UserName { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name="Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+
+        public class Register
+        {
+            [Required]
+            [Display(Name = "Username")]
+            public string UserName { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm Password?")]
+            [Compare("Password", ErrorMessage = "Password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        
+    }
+}
